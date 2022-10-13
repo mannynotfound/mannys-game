3da9c6a42aa10b9d7079554dc108fc3f9f90eb1b
Lochie Axon <lochieaxon@gmail.com>
2022-10-14 08:46:47 +1100

    move: connectkit custom theme into its own file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styles/stars.css';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { ConnectKitProvider, getDefaultClient } from 'connectkit';
 import { chain, createClient, WagmiConfig } from 'wagmi';
+import connectKitTheme from 'connectKitTheme';
 
 import { Header } from 'components';
 import * as Pages from 'pages';
@@ -85,9 +86,6 @@ const Body = () => {
   );
 };
 
-const primaryColor = '#70bf44';
-const secondaryColor = '#f0c925';
-
 const App = () => {
   const client = createClient(
     getDefaultClient({
@@ -102,32 +100,7 @@ const App = () => {
       <ConnectKitProvider
         theme="midnight"
         mode="dark"
-        customTheme={{
-          '--ck-font-family': '"PP Mono"',
-          '--ck-accent-color': secondaryColor,
-          '--ck-accent-text-color': '#000',
-          '--ck-focus-color': secondaryColor,
-          '--ck-spinner-color': secondaryColor,
-          '--ck-body-action-color': secondaryColor,
-          '--ck-body-background': `#0e0e0e`,
-          '--ck-modal-box-shadow': `0 0 0 2px ${primaryColor}`,
-          '--ck-border-radius': '0px',
-
-          '--ck-primary-button-background': '#0e0e0e',
-          '--ck-primary-button-border-radius': '0px',
-          '--ck-primary-button-box-shadow': `0 0 0 2px ${primaryColor}`,
-          '--ck-primary-button-hover-color': '#0e0e0e',
-          '--ck-primary-button-hover-background': primaryColor,
-
-          '--ck-secondary-button-background': '#0e0e0e',
-          '--ck-secondary-button-border-radius': '0px',
-          '--ck-secondary-button-box-shadow': `0 0 0 2px ${secondaryColor}`,
-          '--ck-secondary-button-hover-color': '#0e0e0e',
-
-          '--ck-tooltip-background': '#0e0e0e',
-          '--ck-tooltip-border-radius': '0px',
-          '--ck-tooltip-shadow': `0 0 0 2px ${primaryColor}`,
-        }}
+        customTheme={connectKitTheme}
       >
         <Body />
       </ConnectKitProvider>
